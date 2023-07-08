9da43abb0129edf80ac3cb1afc7457336b36e876
Paul Dicker <paul@pitdicker.nl>
2023-07-08 17:20:04 +0200

    Add benchmark for `DateTime::format_with`

--- benches/chrono.rs
+++ benches/chrono.rs
@@ -169,6 +169,22 @@ fn bench_format(c: &mut Criterion) {
     });
 }
 
+fn bench_format_to_string(c: &mut Criterion) {
+    let dt = Local::now();
+    c.bench_function("bench_format_to_string", |b| {
+        b.iter(|| black_box(dt).format_to_string("%Y-%m-%dT%H:%M:%S%.f%:z"))
+    });
+}
+
+fn bench_format_with(c: &mut Criterion) {
+    let dt = Local::now();
+    let fmt_items: Vec<_> = StrftimeItems::new("%Y-%m-%dT%H:%M:%S%.f%:z").collect();
+    let formatter = DateTime::formatter(&fmt_items).unwrap();
+    c.bench_function("bench_format_with", |b| {
+        b.iter(|| format!("{}", black_box(dt).format_with(&formatter)))
+    });
+}
+
 #[allow(deprecated)]
 fn bench_format_with_items(c: &mut Criterion) {
     let dt = Local::now();
@@ -211,6 +227,8 @@ criterion_group!(
     bench_get_local_time,
     bench_parse_strftime,
     bench_format,
+    bench_format_to_string,
+    bench_format_with,
     bench_format_with_items,
     bench_format_manual,
 );
