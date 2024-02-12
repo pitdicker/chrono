080cd7b96ec311b88f62601dca1ce820bf305c1d
Paul Dicker <paul@pitdicker.nl>
2024-02-12 15:54:44 +0100

    Complete `Error` enum

--- src/error.rs
+++ src/error.rs
@@ -5,6 +5,12 @@ use core::fmt;
 #[non_exhaustive]
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Error {
+    /// There is not enough information to create a date/time.
+    ///
+    /// An example is parsing a string with not enough date/time fields, or the result of a
+    /// time that is ambiguous during a time zone transition (due to for example DST).
+    Ambiguous,
+
     /// A date or datetime does not exist.
     ///
     /// Examples are:
@@ -14,10 +20,28 @@ pub enum Error {
     /// - a leap second on a non-minute boundary.
     DoesNotExist,
 
+    /// Some of the date or time components are not consistent with each other.
+    ///
+    /// An example is parsing 'Sunday 2023-04-21', while that date is a Friday.
+    Inconsistent,
+
     /// One or more of the arguments to a function are invalid.
     ///
     /// An example is creating a `NaiveTime` with 25 as the hour value.
     InvalidArgument,
+
+    /// Character does not match with the expected format (during parsing).
+    ///
+    /// Contains the byte index of the character where the input diverges.
+    InvalidCharacter(u32),
+
+    /// Value is not allowed by the format (during parsing).
+    ///
+    /// Examples are a number that is larger or smaller than the defined range, or the name of a
+    /// weekday, month or timezone that doesn't match.
+    ///
+    /// Contains the byte index pointing at the start of the invalid value.
+    InvalidValue(u32, u8),
 
     /// The result, or an intermediate value necessary for calculating a result, would be out of
     /// range.
@@ -25,14 +49,36 @@ pub enum Error {
     /// An example is a date for the year 500.000, which is out of the range supported by chrono's
     /// types.
     OutOfRange,
+
+    /// All formatting items have been read but there is a remaining input.
+    TooLong,
+
+    /// The format string contains a formatting specifier that is not supported.
+    ///
+    /// Contains the byte index of the formatting specifier within the format string.
+    UnsupportedSpecifier(u32),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            Error::Ambiguous => write!(f, "not enough information for a unique date and time"),
             Error::DoesNotExist => write!(f, "date or datetime does not exist"),
+            Error::Inconsistent => {
+                write!(f, "some of the date or time components are not consistent with each other")
+            }
             Error::InvalidArgument => write!(f, "invalid parameter"),
+            Error::InvalidCharacter(i) => {
+                write!(f, "input doesn't match with the expected format at position {}", i)
+            }
+            Error::InvalidValue(i, len) => {
+                write!(f, "input has a value not allowed by the format at position {} with len {}", i, len)
+            }
             Error::OutOfRange => write!(f, "date outside of the supported range"),
+            Error::TooLong => write!(f, "trailing input"),
+            Error::UnsupportedSpecifier(_) => {
+                write!(f, "format string contains a formatting specifier that is not supported")
+            }
         }
     }
 }
